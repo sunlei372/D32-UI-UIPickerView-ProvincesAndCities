2d24a25d77046138f00922efcc5191d529101fe5
sunlei372 <121648522@qq.com>
2016-03-31 20:11:52 +0800

    nill

---
+++ D32-UI-UIPickerView-ProvincesAndCities/D32-UI-UIPickerView-ProvincesAndCities/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  D32-UI-UIPickerView-ProvincesAndCities
+//
+//  Created by 孙磊 on 16/3/31.
+//  Copyright © 2016年 sunlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ D32-UI-UIPickerView-ProvincesAndCities/D32-UI-UIPickerView-ProvincesAndCities/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  D32-UI-UIPickerView-ProvincesAndCities
+//
+//  Created by 孙磊 on 16/3/31.
+//  Copyright © 2016年 sunlei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
